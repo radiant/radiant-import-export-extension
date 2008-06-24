2022ff315a0c48d0950adebf69eb758c94794c04
Andrew Neil <andrew.jr.neil@gmail.com>
2008-06-24 23:40:31 +0100

    Rather than specifying by hand which models are to be backed up, the Loader module examines the database for a list of all tables. For each table, it attempts to convert the name into a Model (e.g. "page_parts" to PagePart). Tables which do not correspond to a Model are not backed up (e.g. sessions, schema_info and extension_meta). All tables corresponding to a Model are included in the export.yml file. This is especially useful if you are using an extension which adds new tables to the database.

---
+++ lib/loader.rb
@@ -0,0 +1,8 @@
+module Loader
+  def models_from_database
+    tables = ActiveRecord::Base.connection.tables
+    tables.delete "config"
+    models = tables.collect { |table| table.camelize.singularize.constantize rescue nil}.compact
+    models << Radiant::Config
+  end
+end
