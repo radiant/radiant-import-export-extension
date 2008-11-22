2f4e30e7c4f19667f2b07dd0eb1e53069a619f66
Chris Parrish <chris.parrish-gitmail@swankinnovations.com>
2008-11-22 01:06:01 -0700

    Added support for the case where model name and table name are both plural
    In most cases singularizing a table's name yields the model's name but in
    special cases where the model's name is already seen as plural by Rails,
    it expects the table's name to then also be plural (so the model: 'MyData'
    goes with the table: 'my_data')

--- lib/loader.rb
+++ lib/loader.rb
@@ -1,8 +1,12 @@
 module Loader
+  # In most cases singularizing a table's name yields the model's name but in
+  # special cases where the model's name is already seen as plural by rails, it
+  # expects the table's name to then also be plural (so the model: 'MyData' goes
+  # with the table: 'my_data')
   def models_from_database
     tables = ActiveRecord::Base.connection.tables
     tables.delete "config"
-    models = tables.collect { |table| table.camelize.singularize.constantize rescue nil}.compact
+    models = tables.collect { |table| table.camelize.singularize.constantize rescue nil || table.camelize.constantize rescue nil }.compact
     models << Radiant::Config
     models << Radiant::ExtensionMeta
   end
